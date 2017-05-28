d08c317f6eb016010eb6888e4bd0f6161fee5213
Vinoj <vinoj@razorthink.net>
2017-05-28 22:34:07 +0530

    save

--- src/app/App.js
+++ src/app/App.js
@@ -4,23 +4,36 @@ import {
     Header,
     LeftBar,
     RightBar,
-    BottomShadow,
+    Shadow,
     ForceDirectedGraph3d,
-} from './../lib';
+} from '../../lib/index';
 import classNames from 'classnames';
 import * as styles from './style.css';
 
 import data from './data';
 
 export default () => <FixedLayout>
-    <Header/>
+    <Header title={"CUSTOMER"}
+            subtitle="CHURN MODEL"/>
     <LeftBar links={[
         {
             link: '#',
-            style: classNames(styles['nav__link'], styles.dashboard),
+            style: classNames(styles['dashboard']),
         },
-    ]}/>
-    <RightBar/>
-    <BottomShadow/>
-    <ForceDirectedGraph3d data={data}/>
+    ]}>
+        <a href="#" styleName="segments active" id="link-segments"></a>
+        <a href="#" styleName="usage" id="link-usage"></a>
+        <a href="#" styleName="model" id="link-model"></a>
+        <a href="#" styleName="system" id="link-system"></a>
+    </LeftBar>
+    <RightBar>
+        <div styleName="img"></div>
+    </RightBar>
+    <Shadow style={{ bottom: '0px', height: '208px' }}/>
+    <ForceDirectedGraph3d data={data}
+                          tooltip={(c_id = parseInt(Math.random() * 10000000)) => `Customer-${c_id}: Churn Probability: ${parseInt(Math.random() * 100)}%`}
+                          autoColorBy={'user'}
+                          colorField={'color'}
+                          forceEngine={'ngraph'}
+    />
 </FixedLayout>;
